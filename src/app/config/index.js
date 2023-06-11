fa1dc016f6991550bda2497c60a3d654e7c9c395
Egu Chinedu <eguchinedu18@gmail.com>
2023-06-11 23:10:53 +0100

    redeployed

---
+++ src/app/config/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCT7eqBhWlJRT4T2w72dD_AlkcmzqWrwSU",
+  authDomain: "monieplanlogin.firebaseapp.com",
+  projectId: "monieplanlogin",
+  storageBucket: "monieplanlogin.appspot.com",
+  messagingSenderId: "430093344302",
+  appId: "1:430093344302:web:256fd67059026ecc0109bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
